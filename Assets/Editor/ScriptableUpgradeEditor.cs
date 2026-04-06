46e1bd4d49519510985640cdd82f263c75cffc7a
DangerKrip <kostyashka2003@gmail.com>
2026-04-06 13:24:58 +0300

    Fixed upgrade formatted description initialization.

--- Assets/Editor/ScriptableUpgradeEditor.cs
+++ Assets/Editor/ScriptableUpgradeEditor.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Reflection;
-using System.Text.RegularExpressions;
-using UnityEditor;
+﻿using UnityEditor;
 
 [CustomEditor(typeof(ScriptableUpgrade))]
 public class ScriptableUpgradeEditor : Editor
@@ -13,72 +10,13 @@ public class ScriptableUpgradeEditor : Editor
         DrawDefaultInspector();
 
         ScriptableUpgrade script = (ScriptableUpgrade)target;
-
         UpgradeInfo info = script.GetInfo();
-        Upgrade upgrade = script.GetNew();
 
         EditorGUILayout.Space();
-        EditorGUILayout.LabelField("RawDescription Preview:", EditorStyles.boldLabel);
+        EditorGUILayout.LabelField("Live Preview:", EditorStyles.boldLabel);
+        EditorGUILayout.HelpBox(info.FormattedDescription, MessageType.Info);
 
-        string parsedText = ParseDescription(info.RawDescription, upgrade);
-
-        EditorGUILayout.HelpBox(parsedText, MessageType.Info);
-
-        if (EditorGUI.EndChangeCheck())
-        {
-            info.FormattedDescription = parsedText;
-
-            EditorUtility.SetDirty(script);
-        }
 
         serializedObject.ApplyModifiedProperties();
-    }
-
-    private string ParseDescription(string rawText, object source)
-    {
-        if (string.IsNullOrEmpty(rawText) || source == null)
-            return rawText;
-
-        return Regex.Replace(rawText, @"\{(\w+)\}", m =>
-        {
-            string targetName = m.Groups[1].Value;
-            string result = FindMemberValue(source, targetName);
-
-            return result ?? $"<color=red>[{targetName} Not Found]</color>";
-        });
-    }
-
-    private string FindMemberValue(object obj, string name)
-    {
-        if (obj == null)
-            return null;
-
-        Type type = obj.GetType();
-
-        PropertyInfo prop = type.GetProperty(name, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.IgnoreCase);
-        if (prop != null)
-            return prop.GetValue(obj)?.ToString();
-
-        FieldInfo field = type.GetField(name, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.IgnoreCase);
-        if (field != null)
-            return field.GetValue(obj)?.ToString();
-
-        // Recursively search through nested structs and classes
-        foreach (FieldInfo f in type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
-        {
-            if (f.FieldType.IsPrimitive || f.FieldType == typeof(string) || f.FieldType.IsEnum)
-                continue;
-
-            object nestedValue = f.GetValue(obj);
-
-            if (nestedValue == null)
-                continue;
-
-            string found = FindMemberValue(nestedValue, name);
-            if (found != null)
-                return found;
-        }
-
-        return null;
     }
 }
--- Assets/_Scripts/Upgrades/ScriptableUpgrade.cs
+++ Assets/_Scripts/Upgrades/ScriptableUpgrade.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using UnityEngine;
 
 [CreateAssetMenu(menuName = "Upgrade/Upgrade")]
 public class ScriptableUpgrade : ScriptableObject
@@ -6,10 +9,64 @@ public class ScriptableUpgrade : ScriptableObject
     [SerializeField] private UpgradeInfo _upgradeInfo;
     [SerializeReference, SubclassSelector] private Upgrade _upgrade;
 
+    private void OnValidate()
+    {
+        string desc = ParseDescription(_upgradeInfo.RawDescription, _upgrade);
+        _upgradeInfo.FormattedDescription = desc;
+    }
+
     public Upgrade GetNew()
         => Utils.GetInstancedCopyOf(_upgrade);
 
     public UpgradeInfo GetInfo()
         => _upgradeInfo;
+
+    private string ParseDescription(string rawText, object source)
+    {
+        if (string.IsNullOrEmpty(rawText) || source == null)
+            return rawText;
+
+        return Regex.Replace(rawText, @"\{(\w+)\}", m =>
+        {
+            string targetName = m.Groups[1].Value;
+            string result = FindMemberValue(source, targetName);
+
+            return result ?? $"<color=red>[{targetName} Not Found]</color>";
+        });
+    }
+
+    private string FindMemberValue(object obj, string name)
+    {
+        if (obj == null)
+            return null;
+
+        Type type = obj.GetType();
+
+        PropertyInfo prop = type.GetProperty(name, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.IgnoreCase);
+        if (prop != null)
+            return prop.GetValue(obj)?.ToString();
+
+        FieldInfo field = type.GetField(name, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.IgnoreCase);
+        if (field != null)
+            return field.GetValue(obj)?.ToString();
+
+        // Recursively search through nested structs and classes
+        foreach (FieldInfo f in type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
+        {
+            if (f.FieldType.IsPrimitive || f.FieldType == typeof(string) || f.FieldType.IsEnum)
+                continue;
+
+            object nestedValue = f.GetValue(obj);
+
+            if (nestedValue == null)
+                continue;
+
+            string found = FindMemberValue(nestedValue, name);
+            if (found != null)
+                return found;
+        }
+
+        return null;
+    }
 }
 
